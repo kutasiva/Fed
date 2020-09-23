a5d9a66664f2e99092cbc48a6ebaf17387d9cdd6
Kutasov, Ivan <ivan.kutasov@merck.com>
2020-09-23 14:38:24 +0200

    Add Project

---
+++ FED/FED/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FED
+//
+//  Created by Kutasov, Ivan on 23/09/2020.
+//  Copyright © 2020 Kutasov, Ivan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
