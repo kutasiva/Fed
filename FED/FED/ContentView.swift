c9b64c762188b17afd937837c51a4646ca388f16
Kutasov, Ivan <ivan.kutasov@merck.com>
2020-09-23 15:02:22 +0200

    ADD button

--- FED/FED/ContentView.swift
+++ FED/FED/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, World!")
+        Button(
+            "Open photo Library",
+            action: {
+            }
+        )
+            .padding()
     }
 }
 
